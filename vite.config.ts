84919d1416a7146da7bee6bac8a5c1ce40ab7e15
Samuel Hermany <samuelhermany@hotmail.com>
2025-08-25 22:00:22 -0300

    fix: Ajustado caminho para o gh-Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'infnet-pb-blog',
+  base: 'front-end_monolito-blog',
   // server: {
   //   open: true, // Isso abre o navegador automaticamente
   //   port: 3000, // Define a porta que você quer usar
